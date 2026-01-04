c7051126cf278d3e3a0a8e85e0e3afdd514dffda
Christopher Kaster <me@atomicptr.de>
2026-01-04 12:40:41 +0100

    add colspan/rowspan to td element

--- src/Html5/Elements/Td.php
+++ src/Html5/Elements/Td.php
@@ -3,11 +3,40 @@
 namespace Berry\Html5\Elements;
 
 use Berry\Html5\HtmlTag;
+use InvalidArgumentException;
 
 class Td extends HtmlTag
 {
     public function __construct()
     {
         parent::__construct('td');
+    }
+
+    /**
+     * Sets the number of columns a cell should span.
+     *
+     * @throws InvalidArgumentException If colspan is less than 1.
+     */
+    public function colspan(int $colspan): static
+    {
+        if ($colspan <= 0) {
+            throw new InvalidArgumentException('colspan has to be at least 1');
+        }
+
+        return $this->attr('colspan', strval($colspan));
+    }
+
+    /**
+     * Sets the number of rows a cell should span.
+     *
+     * @throws InvalidArgumentException If rowspan is less than 1.
+     */
+    public function rowspan(int $rowspan): static
+    {
+        if ($rowspan <= 0) {
+            throw new InvalidArgumentException('rowspan has to be at least 1');
+        }
+
+        return $this->attr('rowspan', strval($rowspan));
     }
 }
--- tests/Html5/Elements/TableTest.php
+++ tests/Html5/Elements/TableTest.php
@@ -17,3 +17,23 @@ test('table renders with child', function () {
             ->toString()
     )->toBe('<table><thead><th>Col</th></thead><tbody><tr><td>Hello, Table</td></tr></tbody><tfoot><tr><td>Foot</td></tr></tfoot></table>');
 });
+
+test('table with colspan and rowspan', function () {
+    $html = td()
+        ->colspan(2)
+        ->rowspan(3)
+        ->text('Spanning Cell')
+        ->toString();
+
+    expect($html)->toBe('<td colspan="2" rowspan="3">Spanning Cell</td>');
+});
+
+test('colspan throws exception for values less than 1', function () {
+    expect(fn() => td()->colspan(0))
+        ->toThrow(InvalidArgumentException::class, 'colspan has to be at least 1');
+});
+
+test('rowspan throws exception for values less than 1', function () {
+    expect(fn() => td()->rowspan(-1))
+        ->toThrow(InvalidArgumentException::class, 'rowspan has to be at least 1');
+});
