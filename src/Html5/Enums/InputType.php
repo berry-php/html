c5348d46f63d4332a5067ffcab4e86e96e17a06c
Christopher Kaster <me@atomicptr.de>
2026-01-04 13:55:25 +0100

    add missing InputType fields

--- src/Html5/Enums/InputType.php
+++ src/Html5/Enums/InputType.php
@@ -4,16 +4,26 @@ namespace Berry\Html5\Enums;
 
 enum InputType: string
 {
-    case Text = 'text';
-    case Password = 'password';
-    case Checkbox = 'checkbox';
-    case Radio = 'radio';
-    case File = 'file';
-    case Submit = 'submit';
-    case Reset = 'reset';
     case Button = 'button';
-    case Hidden = 'hidden';
-    case Email = 'email';
-    case Number = 'number';
+    case Checkbox = 'checkbox';
+    case Color = 'color';
     case Date = 'date';
+    case DateTimeLocal = 'datetime-local';
+    case Email = 'email';
+    case File = 'file';
+    case Hidden = 'hidden';
+    case Image = 'image';
+    case Month = 'month';
+    case Number = 'number';
+    case Password = 'password';
+    case Radio = 'radio';
+    case Range = 'range';
+    case Reset = 'reset';
+    case Search = 'search';
+    case Submit = 'submit';
+    case Tel = 'tel';
+    case Text = 'text';
+    case Time = 'time';
+    case Url = 'url';
+    case Week = 'week';
 }
