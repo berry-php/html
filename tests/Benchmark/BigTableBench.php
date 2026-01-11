7c65debc31df633f75009f5a3dd2b10b4832af7a
Christopher Kaster <me@atomicptr.de>
2026-01-11 18:28:23 +0100

    add more benchmarks comparing the renderers

--- tests/Benchmark/BigTableBench.php
+++ tests/Benchmark/BigTableBench.php
@@ -2,7 +2,10 @@
 
 namespace Berry\Tests\Benchmark;
 
-use Berry\Rendering\StringRenderer;
+use Berry\Rendering\ArrayBufferRenderer;
+use Berry\Rendering\DirectOutputRenderer;
+use Berry\Rendering\Renderer;
+use Berry\Rendering\ResourceRenderer;
 
 use function Berry\Html5\table;
 use function Berry\Html5\td;
@@ -10,7 +13,7 @@ use function Berry\Html5\tr;
 
 class BigTableBench
 {
-    private function buildTable(int $rows, int $cols, ?StringRenderer $renderer = null): void
+    private function buildTable(int $rows, int $cols, Renderer $renderer): void
     {
         $table = table();
 
@@ -24,24 +27,64 @@ class BigTableBench
             $table->child($tr);
         }
 
-        $table->toString();
+        $table->render($renderer);
     }
 
     /**
      * @Revs(100)
      * @Iterations(5)
      */
-    public function benchBuild100x100Table(): void
+    public function benchBuild100x100TableArrayBufferRenderer(): void
     {
-        $this->buildTable(100, 100);
+        $this->buildTable(100, 100, new ArrayBufferRenderer());
+    }
+
+    /**
+     * @Revs(100)
+     * @Iterations(5)
+     */
+    public function benchBuild100x100TableResourceRenderer(): void
+    {
+        $this->buildTable(100, 100, ResourceRenderer::temp());
+    }
+
+    /**
+     * @Revs(100)
+     * @Iterations(5)
+     */
+    public function benchBuild100x100TableDirectOutputRenderer(): void
+    {
+        ob_start();
+        $this->buildTable(100, 100, new DirectOutputRenderer());
+        ob_end_clean();
     }
 
     /**
      * @Revs(10)
      * @Iterations(1)
      */
-    public function benchBuild1000x1000Table(): void
+    public function benchBuild1000x1000TableArrayBufferRenderer(): void
     {
-        $this->buildTable(1000, 1000);
+        $this->buildTable(1000, 1000, new ArrayBufferRenderer());
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(1)
+     */
+    public function benchBuild1000x1000TableResourceRenderer(): void
+    {
+        $this->buildTable(1000, 1000, ResourceRenderer::temp());
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(1)
+     */
+    public function benchBuild1000x1000TableDirectOutputRenderer(): void
+    {
+        ob_start();
+        $this->buildTable(1000, 1000, new DirectOutputRenderer());
+        ob_end_clean();
     }
 }
