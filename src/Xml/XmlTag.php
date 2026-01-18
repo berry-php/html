2d2ed80a5b8418ff1da23692d03046bee879e8de
Christopher Kaster <me@atomicptr.de>
2026-01-18 13:32:18 +0100

    remove inspector support from xml

--- src/Xml/XmlTag.php
+++ src/Xml/XmlTag.php
@@ -3,16 +3,13 @@
 namespace Berry\Xml;
 
 use Berry\Contract\HasChildrenContract;
-use Berry\Contract\HasInspectorContract;
 use Berry\Contract\HasTextContract;
-use Berry\Tag;
 use Berry\Traits\HasChildren;
-use Berry\Traits\HasInspector;
 use Berry\Traits\HasText;
+use Berry\Tag;
 
-abstract class XmlTag extends Tag implements HasChildrenContract, HasTextContract, HasInspectorContract
+abstract class XmlTag extends Tag implements HasChildrenContract, HasTextContract
 {
     use HasChildren;
     use HasText;
-    use HasInspector;
 }
